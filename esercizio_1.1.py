9ed4b5098a19df74defd1c4a2406762762c61d69
poulbag <70801993+poulbag@users.noreply.github.com>
2021-02-24 11:14:52 +0100

    Create esercizio_1.1.py

---
+++ esercizio_1.1.py
@@ -0,0 +1,19 @@
+# THE BRIDGE OF DEATH - EXERCISE 1.1
+
+print("What is your name?")
+name = input()
+if (name == "Lancelot"):
+    print("What is your quest?")
+    quest = input ()
+    if (quest == "Holy Grail"):
+        print("What is your favorite color?")
+        color = input()
+        if(color == "Blue"):
+            print("Right. Off you go.")
+        else:
+            print("Down into the Gorge of Ethernal Peril!")
+
+    else:
+        print("Down into the Gorge of Ethernal Peril!")
+else:
+    print("Down into the Gorge of Ethernal Peril!")
